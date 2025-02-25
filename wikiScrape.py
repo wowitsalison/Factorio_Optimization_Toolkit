36afc82b3c838c8ace2623dedc0cf002a93e5b77
wowitsalison <aliatmra@gmail.com>
2025-02-24 22:34:57 -0600

    Create initial web scraper- gets materials off origin page

--- wikiScrape.py
+++ wikiScrape.py
@@ -1,3 +1,56 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+
+# Link to the rocket silo factorio wiki page
+origin_link = "https://wiki.factorio.com/Rocket_silo"
+
+# Include a User-Agent header to identify the client
+headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36" }
+
+# Initialize material dictionary to add materials to
+materials = {
+    "Rocket silo" : 1
+}
+
+# Function to convert quantity string to int
+def convert_quantity(quantity):
+    # If "k" is in the quantity, multiply by 1000
+    if 'k' in quantity:
+        return int(quantity.replace('k', '')) * 1000
+    # If there's no "k" return  integer value
+    return int(quantity)
+
+def get_materials(material_link, headers):
+    # Fetch the wiki page
+    wiki_response = requests.get(material_link, headers=headers)
+    try: 
+        wiki_soup = BeautifulSoup(wiki_response.text, 'html.parser')
+        # Find materials section
+        material_links = wiki_soup.find('td', class_='infobox-vrow-value')
+        # Get links from materials section
+        material_links = material_links.find_all('a', href=True, title=True)
+
+        # Get material name, link, and quantity 
+        for material in material_links[1:-1]:   # Skip first and last- time and current material
+            title = material['title']
+            material_url = "https://wiki.factorio.com" + material['href']
+            quantity = material.find_next('div', class_='factorio-icon-text').text.strip()
+            quantity = convert_quantity(quantity)
+
+            # Check if material is already in dictionary
+            if title in materials:
+                # If it is, increase the quantity
+                materials[title] += quantity
+            else:
+                # If it's not, add it
+                materials[title] = quantity
+
+            print(f"Material: {title}, Link: {material_url}, Quantity: {quantity}")
+
+
+    except Exception as e:
+        print(f"Error fetching wiki page: {e}")
+
+if __name__ == "__main__":
+    get_materials(origin_link, headers)
